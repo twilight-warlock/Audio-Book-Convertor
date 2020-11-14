0591bcd674289be6cc48558ee4a064cc1d5cba99
twilight-warlock <devansh.ts@somaiya.edu>
2020-11-15 03:44:00 +0530

    Try 2,facing issue of last lines

--- read.py
+++ read.py
@@ -1,48 +1,78 @@
 import PyPDF2
 from gtts import gTTS
+from time import sleep
+import re
 
 # creating a pdf file object
 pdfFileObj = open('The-Compound-Effect.pdf', 'rb')
-chapter1 = open("chap1.txt", "a+")
+language = 'en'
 
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+total = pdfReader.numPages
 
-# Entering page numbers for each chapter (start,end+1)
-for i in range(24, 42):
-    pageText = pdfReader.getPage(i).extractText()
-    chapter1.write(pageText)
-    chapter1.write("\n\n\n")
+numOfChaps = int(input("Enter number of chapters: "))
+pageNums = []
+chapName = ["THE COMPOUND EFFECT"]
 
-pdfFileObj.close()
-chapter1.close()
+for i in range(numOfChaps):
+    print("Chapter", i+1, "starts at", end="")
+    pageNums.append(int(input(" : "))-1)
 
-# ------------------- Cleaning the text -------------------
-with open("./chap1.txt", "r") as file:
-    with open("chap1_cleaned.txt", "a+") as chap1:
-        for line in file:
-            line = line.strip()
 
-            # Removing extra lines coming from PyPDF2
-            if(line.startswith("Chapter_1.indd")):
-                line = file.readline()
-                line = file.readline()
-                line = file.readline()
-                line = file.readline()
-                line = file.readline()
+pageNums.append(total)
+# print(pageNums)
 
-            # Removing top line of Book Name
-            if("THE COMPOUND EFFECT" in line):
-                line = file.readline()
-            chap1.write(line)
+for j in range(len(pageNums)-1):
+
+    for i in range(pageNums[j], pageNums[j+1]-1):
+        pageText = pdfReader.getPage(i).extractText()
+        chapter1 = open("chap"+str(j+1)+".txt", "a+",
+                        encoding='utf-8', errors='ignore')
+        chapter1.write(pageText)
+    # chapter1.close()
+    # ------------------- Cleaning the text -------------------
+    with open("./chap"+str(j+1)+".txt", "r", encoding='utf-8', errors='ignore') as file:
+        with open("chap"+str(j+1)+"_cleaned.txt", "a+",  encoding='utf-8', errors='ignore') as chap1:
+            for line in file:
+                line = line.strip()
+
+                # Removing extra lines coming from PyPDF2
+                if(line.startswith("Chapter_"+str(j+1)+".indd")):
+                    # line = file.readline()
+                    continue
+
+                # Adding Chapter Names
+                if(line.startswith("CHAPTER")):
+                    chapName.append(line[9:].strip())
+                    chap1.write(line[:9]+" "+line[9:]+"\n")
+                    continue
+                # Removing top line of chapter1 Name
+                if("THE COMPOUND EFFECT" in line):
+                    line = line[line.index(
+                        "THE COMPOUND EFFECT")+len("THE COMPOUND EFFECT"):]
+
+                if("Chapter_"+str(j+1)+".indd" in line):
+                    line = line[:line.index("Chapter_"+str(j+1)+".indd")]
+
+                if re.search("\d{2} .M.+", line):
+                    word = "AM" if "AM" in line else "PM"
+                    for e in chapName:
+                        if(e in line):
+                            word = e
+                            break
+                    line = line[line.index(word)+len(word):]
+                    if(line.isnumeric()):
+                        continue
+
+                chap1.write(line+" ")
+# pdfFileObj.close()
+
 
 # ------------------- Converting to voice -------------------
-with open("chap1_cleaned.txt") as file:
-    language = 'en'
-    myobj = gTTS(text=file.read(), lang=language, slow=False)
-    myobj.save("chap1.mp3")
-
-# import os
-# os.remove("demofile.txt")
-# os.mkdir()
-# os.getcwd()
+# for j in range(len(pageNums)-1):
+#     with open("chap"+str(j+1)+"_cleaned.txt", encoding='utf-8', errors='ignore') as file:
+#         myobj = gTTS(text=file.read(), lang=language, slow=False)
+#         myobj.save("chap"+str(j+1)+".mp3")
+#         sleep(1)
+#         print(str(j+1)+" audio file created")
